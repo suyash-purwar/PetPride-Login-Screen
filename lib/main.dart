5d83b001a3e3713ace5b3811377e224b0a558788
Suyash <purwarsuyash06@gmail.com>
2019-05-31 01:17:03 +0530

    Changed image assets and made login screen background and made primary headline

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primaryColor: Color.fromRGBO(146, 220, 229, 1.0),
       )
     );
   }
@@ -23,10 +23,65 @@ class LoginPageState extends State<LoginPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        
-      ),
+      body: SafeArea(
+        child:Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image(
+              image: AssetImage("assets/images/backgroundimg.jpg"),
+              fit: BoxFit.cover,
+              color: Theme.of(context).primaryColor.withOpacity(1),
+              colorBlendMode: BlendMode.softLight
+            ),
+
+            Center(
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.white.withOpacity(0.5)
+              )
+            ),
+
+            Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 100),
+                  child: Text(
+                    "Pet Pride",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 45,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w900,
+                      letterSpacing: 2.5,
+                      height: 1
+                    )
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 0),
+                  child: Text(
+                    "OF NEW YORK",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      height: 0.5,
+                      color: Colors.black87,
+                      fontSize: 15,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 5,
+                      wordSpacing: 5
+                    )
+                  ),
+                ),
+              ],
+              
+            ),
+
+          ]
+        ),
+      )
     );
   }
 }
